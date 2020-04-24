11a10bbe6a2c9c631ed8db8c5f0d2bccd7f2fc59
vynsss <vickyvanessa02@yahoo.com>
2020-04-24 17:53:58 +0700

    Cart function and APi

--- Order/app/Http/Controllers/CartController.php
+++ Order/app/Http/Controllers/CartController.php
@@ -2,11 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Cart;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CartController extends Controller
 {
     public function show(){
-        
+        $stmt = Cart::All()->where('status_id', 1);
+        echo json_encode(array(
+            "cart" => $stmt
+        ));
+    }
+
+    public function add(Request $request){
+        $product = $request->input('product_id');
+        $qty = $request->input('quantity');
+        $user = $request->input('user_id');
+        $date = Carbon::now()->toDateString();
+
+        $stmt = DB::table('carts')
+            ->insert([
+                'product_id' => $product,
+                'quantity' => $qty,
+                'user_id' => $user,
+                'date' => $date,
+                ]);
+
+        echo json_encode(array(
+            "success" => $stmt,
+            "message" => "Successfully inserted data"
+        ));
     }
 }
