5aff25a5fe2c27a56ce94f7ac2e071443e9f9c66
vynsss <vickyvanessa02@yahoo.com>
2020-06-09 21:12:52 +0700

    add and include header file

--- Frontend/cutomer_page.php
+++ Frontend/cutomer_page.php
@@ -1,117 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Bali Products - Corporate Meeting & Events</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,700,900|Oswald:400,700">
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
-
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
-    <link rel="stylesheet" href="css/jquery-ui.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="css/mediaelementplayer.css">
-    <link rel="stylesheet" href="css/animate.css">
-    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
-    <link rel="stylesheet" href="css/fl-bigmug-line.css">
-
-
-    <link rel="stylesheet" href="css/aos.css">
-
-    <link rel="stylesheet" href="css/style.css">
-
-</head>
-<body>
-
-
-
-
-<div class="site-wrap">
-
-    <div class="site-navbar mt-4">
-        <div class="container py-1">
-            <div class="row align-items-center">
-                <div class="col-8 col-md-8 col-lg-12">
-                    <h1 class="mb-0"><a href="home.html" class="text-white h2 mb-0"><strong>Bali Products<span class="text-primary">.</span></strong></a></h1>
-                    <h4 style="text-align: right; color: rgba(255, 255, 255, 0.5); letter-spacing: 4px "><span><a href="#">Login</a></span> / <span><a href="#">Sign In</a></span><span style="color: rgba(255, 255, 255, 0)">---</span> | <span style="color: rgba(255, 255, 255, 0)">---</span><span><a href="#" style="text-align: right"><span class="icon-shopping-cart"></span></a></span></h4>
-                    <!--vicky ini buat klo udh login jdi namanya langsung ada gitu-->
-                    <!--<h4 style="text-align: right; color: rgba(255, 255, 255, 0.5); letter-spacing: 4px "><span style="color: #e3c4a8">Vicky Vanessa</span><span style="color: rgba(255, 255, 255, 0)">-&#45;&#45;</span> | <span style="color: rgba(255, 255, 255, 0)">-&#45;&#45;</span><span><a href="#" style="text-align: right"><span class="icon-shopping-cart"></span></a></span></h4>-->
-
-                </div>
-                <div class="col-4 col-md-4 col-lg-12">
-                    <nav class="site-navigation text-right text-md-right" role="navigation">
-
-                        <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a></div>
-
-                        <ul class="site-menu js-clone-nav d-none d-lg-block">
-                            <li>
-                                <a href="home.html">Home</a>
-                            </li>
-                            <li class="has-children">
-                                <a href="shop.html">Shop</a>
-                                <ul class="dropdown arrow-top">
-                                    <li><a href="home_wares.html">Home Wares</a></li>
-                                    <li><a href="soft_furniture.html">Soft Furniture</a></li>
-                                    <li><a href="tableware.html">Tableware</a></li>
-                                    <li><a href="fashion.html">Fashion</a></li>
-                                    <li><a href="home_furniture.html">Home Furniture</a></li>
-                                    <li><a href="clothing.html">Clothings</a></li>
-                                </ul>
-                            </li>
-                            <li class="has-children active">
-                                <a href="services.html">Services</a>
-                                <ul class="dropdown arrow-top">
-                                    <li><a href="private_label.html">Private Label</a></li>
-                                    <li><a href="interior_solution.html">Bali Style Interior Solutions</a></li>
-                                    <li><a href="corporate_meeting.html">Corporate Meetings & Events</a></li>
-                                    <li><a href="custom_made.html">Custom Made & Designs</a></li>
-                                    <li><a href="exports_quality.html">Exports & Quality Control</a></li>
-                                    <li><a href="wholesale_sourcing.html">Wholesale Sourcing Tour</a></li>
-                                    <li><a href="service_fee.html">Service Fees</a></li>
-                                    <li><a href="terms.html">Terms</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="blog.html">Blog</a></li>
-                            <li class="has-children">
-                                <a href="about_us.html">About</a>
-                                <ul class="dropdown arrow-top">
-                                    <li><a href="about_us.html">About Us</a></li>
-                                    <li><a href="testimonial.html">Testimonials</a></li>
-                                    <li><a href="customer.html">Our Customers</a></li>
-                                    <li><a href="team.html">Our Team</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="contact.html">Contact Us</a>
-                            </li>
-                            <li>
-                                <div class="input-group mb-3" >
-                                    <input type="text" class="form-control bg-transparent" placeholder="Search"
-                                           aria-label="Search" aria-describedby="button-addon2" style="background-color: rgba(255, 255, 255, 0.2);">
-                                    <div class="input-group-append" >
-                                        <button class="btn btn-primary rounded-top-right-0" type="button" id="button-addon2">Search</button>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="site-mobile-menu">
-        <div class="site-mobile-menu-header">
-            <div class="site-mobile-menu-close mt-3">
-                <span class="icon-close2 js-menu-toggle"></span>
-            </div>
-        </div>
-        <div class="site-mobile-menu-body"></div>
-    </div> <!-- .site-mobile-menu -->
+<?php
+    include 'include/navbar.html';
+?>
 
     <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url('images/balitemple.jpg');"
          data-aos="fade" data-stellar-background-ratio="0.5" data-aos="fade">
@@ -119,7 +8,7 @@
             <div class="row align-items-center justify-content-center">
                 <div class="col-md-7 text-center" data-aos="fade-up" data-aos-delay="400">
 
-                    <h1 class="text-white">Corporate Meeting & Events</h1>
+                    <h1 class="text-white">Customer</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                 </div>
             </div>
@@ -131,30 +20,41 @@
             <!--<div class="row">-->
             <div class="col-lg-12">
 
-                <h1 class="mb-5" style="text-align: center">Bali Corporate Meetings, Events and Conferences</h1>
-
-                <h5 class="mb-5" style="text-align: center">We offer event and conference organizers with corporate meetings promotional gifts, event support products and event on-site support staff. We provide corporate and promotional meetings, incentives, conferences and event (MICE) organizers with a variety of on-site product and staffing services to support the successful conduct of their event here in Bali.</h5>
-
                 <figure>
-                <img src="images/hero_bg_1.jpg" alt="Image" class="img-fluid">
-                <!--<figcaption>This is an image</figcaption>-->
+                    <img src="images/hero_bg_1.jpg" alt="Image" class="img-fluid">
+                    <!--<figcaption>This is an image</figcaption>-->
                 </figure>
+                <h1 class="mb-5" style="text-align: center">Looking for a private labels supplier for your product branding?</h1>
 
-                <h5 class="mb-5">Corporate promotional gifts</h5>
+                <h5 class="mb-5" style="text-align: center">Want to create your own branded private labels products and designs,
+                    but don’t know who to trust? Looking for a custom made product manufacturer who you can understand, and who
+                    understands what you want? You can stop looking.</h5>
 
-                <p>Bali Products can provide and source a variety of Bali made traditional products to give away as Corporate Promotional Gifts for any company or conference organizer conducting an annual corporate meeting or event here in Bali, at many of our Bali Resorts or Hotels.  The type of corporate or event promotional gifts we provide range from wooden handicraft, gold or silver souvenirs, batik clothing, traditional sarongs, silver or wooden artefacts, jewelry, paintings, shell or horn handicraft etc.</p>
+                <p>Whether you are a start up company or a well known brand, you will need to find a manufacturer to make your
+                    products to include your own brand name. In order to do this, and remain competitive in the market, more and
+                    more companies are turning to overseas manufacturers to produce their products.</p>
 
-                <p>All our corporate promotional gifts come in beautifully crafted boxes or containers. All our corporate promotional gifts can be labeled or inscribed with either the name of the company, the event or conference, or the event organiser. We can also organise gift bags and gift holders with swing tags or labels made to your design.</p>
+                <p>When looking for a supplier or manufacturer, you need to be careful, as it is vital to find one that will not
+                    turn around and copy your designs. It is important to work with someone that you can trust, who has integrity
+                    and will protect your name.</p>
 
-                <p>All our corporate promotional gifts come in beautifully crafted boxes or containers. All our corporate promotional gifts can be labeled or inscribed with either the name of the company, the event or conference, or the event organiser. We can also organise gift bags and gift holders with swing tags or labels made to your design.</p>
+                <p>We at Bali Products can help you find just what you need. We have been based in Bali for many years, and have
+                    built up relations with many reliable manufacturers on the island. We know the best places to go to find the
+                    best products.</p>
 
-                <p class="mb-5">All our corporate promotional gifts come in beautifully crafted boxes or containers. All our corporate promotional gifts can be labeled or inscribed with either the name of the company, the event or conference, or the event organiser. We can also organise gift bags and gift holders with swing tags or labels made to your design.</p>
+                <p class="mb-5">We can help you launch your line or collection, or take an existing line to the next level. We
+                    are here to do everything we can to help you and your business grow. We are here to help with advice and
+                    solutions to all your needs. To find out more about this please take a look at our Private Label Solutions
+                    page.</p>
 
-                <h5 class="mb-5">Corporate event support products</h5>
+                <h5 class="mb-5">private labels manufacturing</h5>
 
-                <p>In addition to supplying corporate and event, conference and meetings promotional gifts, Bali Products can also supply all of the necessary meeting, incentive, conference and event support material and products required by overseas event organizers to manage and run their event here in Bali.</p>
+                <p>The beauty of Bali is that the manufacturing is on a smaller scale to other countries such as China and India.
+                    By going to smaller production houses to produce your line, you can be more sure of keeping your brand exclusive.</p>
 
-                <p>Examples of the type of event support materials and products we can supply include umbrellas, signs & stands, staff t-shirts & sweatshirts, sandals, raincoats, back packs, hats, display stands etc. All branded with your own company logo – or your clients logo.</p>
+                <p>There are plenty of product manufacturers on the island, all offering their own particular specialties. We have
+                    spent years building up relations with many of these facilities, and have very strict guidelines when choosing
+                    who to use for production.</p>
 
                 <p>Bali has a wide range of products on offer, and various ways to make it your own. To learn more about the types
                     of manufacturing that can be found here, please take a look at our Private Label Manufacturers and Private
@@ -234,7 +134,6 @@
                 </div>
             </div>
         </div>
-
         <footer class="site-footer">
             <div class="container">
                 <div class="row">
